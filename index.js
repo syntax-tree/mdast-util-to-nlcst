2fbe2a4f6c52873316a5f55707c78c773a7cf210
Titus Wormer <tituswormer@gmail.com>
2016-09-18 13:39:29 +0200

    Refactor code-style
    * Remove superfluous files;
    * Remove unneeded Node targets from Travis;
    * Replace `istanbul` with `nyc`;
    * Replace `eslint`, `jscs` with `xo`.

--- index.js
+++ index.js
@@ -8,24 +8,177 @@
 
 'use strict';
 
-/* eslint-env commonjs */
-
 /* Dependencies. */
 var repeat = require('repeat-string');
 var vfileLocation = require('vfile-location');
 var toString = require('nlcst-to-string');
 
+/* Expose. */
+module.exports = toNLCST;
+
 /* Map of ignored mdast nodes: nodes which have no (simple)
  * representation in NLCST. */
 var IGNORE = {
-    horizontalRule: true,
-    table: true,
-    tableRow: true,
-    tableCell: true
+  horizontalRule: true,
+  table: true,
+  tableRow: true,
+  tableCell: true
 };
 
 /* Constants. */
 var C_NEWLINE = '\n';
+
+/**
+ * Transform `ast` into `nlcst`.
+ *
+ * @param {Node} tree - MDAST node.
+ * @param {File} file - Virtual file.
+ * @param {Parser|Function} Parser - (Instance of) NLCST
+ *   parser.
+ * @return {NLCSTNode} - NLCST.
+ */
+function toNLCST(tree, file, Parser) {
+  var parser;
+  var location;
+
+  /* Warn for invalid parameters. */
+  if (!tree || !tree.type) {
+    throw new Error('mdast-util-to-nlcst expected node');
+  }
+
+  if (!file || !file.messages) {
+    throw new Error('mdast-util-to-nlcst expected file');
+  }
+
+  /* Construct parser. */
+  if (!Parser) {
+    throw new Error('mdast-util-to-nlcst expected parser');
+  }
+
+  location = vfileLocation(file);
+
+  if (
+    !tree.position ||
+    !tree.position.start ||
+    !tree.position.start.column ||
+    !tree.position.start.line
+  ) {
+    throw new Error('mdast-util-to-nlcst expected position on nodes');
+  }
+
+  parser = 'parse' in Parser ? Parser : new Parser();
+
+  /* Transform mdast into NLCST tokens, and pass these
+   * into `parser.parse` to insert sentences, paragraphs
+   * where needed. */
+  return parser.parse(one(tree, null, null, file, location, parser));
+}
+
+/**
+ * Convert `node` into NLCST.
+ *
+ * @param {MDASTNode} node - Node.
+ * @param {number?} index - Position of `node` in `parent`.
+ * @param {MDASTNode?} parent - Parent node of `node`.
+ * @param {File} file - Virtual file.
+ * @param {Object} location - Bound location info.
+ * @param {Parser} parser - NLCST parser.
+ * @return {Array.<NLCSTNode>?} - A list of NLCST nodes, if
+ *   `node` could be converted.
+ */
+function one(node, index, parent, file, location, parser) {
+  var type = node.type;
+  var pos = node.position;
+  var start = location.toOffset(pos.start);
+  var end = location.toOffset(pos.end);
+  var replacement;
+
+  if (type in IGNORE) {
+    return null;
+  }
+
+  if (node.children) {
+    replacement = all(node, file, location, parser);
+  } else if (
+    type === 'image' ||
+    type === 'imageReference'
+  ) {
+    replacement = patch(
+      parser.tokenize(node.alt), location, start + 2
+    );
+  } else if (
+    type === 'text' ||
+    type === 'escape'
+  ) {
+    replacement = patch(
+      parser.tokenize(node.value), location, start
+    );
+  } else if (node.type === 'break') {
+    replacement = patch([
+      parser.tokenizeWhiteSpace('\n')
+    ], location, start);
+  } else if (node.type === 'inlineCode') {
+    replacement = patch([parser.tokenizeSource(
+      file.toString().slice(start, end)
+    )], location, start);
+  }
+
+  return replacement || null;
+}
+
+/**
+ * Convert all nodes in `parent` (mdast) into NLCST.
+ *
+ * @param {MDASTNode} parent - Parent node.
+ * @param {File} file - Virtual file.
+ * @param {Object} location - Bound location info.
+ * @param {Parser} parser - NLCST parser.
+ * @return {Array.<NLCSTNode>} - Concatenation of calling
+ *   `one` on each MDASTNode in `parent`.
+ */
+function all(parent, file, location, parser) {
+  var children = parent.children;
+  var length = children && children.length;
+  var index = -1;
+  var result = [];
+  var child;
+  var node;
+  var pos;
+  var prevEndLine;
+  var prevOffset;
+  var endLine;
+
+  while (++index < length) {
+    node = children[index];
+    pos = node.position;
+    endLine = pos.start.line;
+
+    if (prevEndLine && endLine !== prevEndLine) {
+      child = parser.tokenizeWhiteSpace(
+        repeat(C_NEWLINE, endLine - prevEndLine)
+      );
+
+      patch([child], location, prevOffset);
+
+      if (child.value.length < 2) {
+        child.value = repeat(C_NEWLINE, 2);
+      }
+
+      result.push(child);
+    }
+
+    child = one(node, index, parent, file, location, parser);
+
+    if (child) {
+      result = result.concat(child);
+    }
+
+    prevEndLine = pos.end.line;
+    prevOffset = pos.end.offset;
+  }
+
+  return result;
+}
 
 /**
  * Patch a position on each node in `nodes`.
@@ -42,185 +195,30 @@ var C_NEWLINE = '\n';
  * @return {Array.<NLCSTNode>} - `nodes`.
  */
 function patch(nodes, location, offset) {
-    var length = nodes.length;
-    var index = -1;
-    var start = offset;
-    var children;
-    var node;
-    var end;
+  var length = nodes.length;
+  var index = -1;
+  var start = offset;
+  var children;
+  var node;
+  var end;
 
-    while (++index < length) {
-        node = nodes[index];
-        children = node.children;
+  while (++index < length) {
+    node = nodes[index];
+    children = node.children;
 
-        if (children) {
-            patch(children, location, start);
-        }
-
-        end = start + toString(node).length;
-
-        node.position = {
-            start: location.toPosition(start),
-            end: location.toPosition(end)
-        }
-
-        start = end;
+    if (children) {
+      patch(children, location, start);
     }
 
-    return nodes;
+    end = start + toString(node).length;
+
+    node.position = {
+      start: location.toPosition(start),
+      end: location.toPosition(end)
+    };
+
+    start = end;
+  }
+
+  return nodes;
 }
-
-/**
- * Convert all nodes in `parent` (mdast) into NLCST.
- *
- * @param {MDASTNode} parent - Parent node.
- * @param {File} file - Virtual file.
- * @param {Object} location - Bound location info.
- * @param {Parser} parser - NLCST parser.
- * @return {Array.<NLCSTNode>} - Concatenation of calling
- *   `one` on each MDASTNode in `parent`.
- */
-function all(parent, file, location, parser) {
-    var children = parent.children;
-    var length = children && children.length;
-    var index = -1;
-    var result = [];
-    var child;
-    var node;
-    var pos;
-    var prevEndLine;
-    var prevOffset;
-    var endLine;
-
-    while (++index < length) {
-        node = children[index];
-        pos = node.position;
-        endLine = pos.start.line;
-
-        if (prevEndLine && endLine !== prevEndLine) {
-            child = parser.tokenizeWhiteSpace(
-                repeat(C_NEWLINE, endLine - prevEndLine)
-            );
-
-            patch([child], location, prevOffset);
-
-            if (child.value.length < 2) {
-                child.value = repeat(C_NEWLINE, 2);
-            }
-
-            result.push(child);
-        }
-
-        child = one(node, index, parent, file, location, parser);
-
-        if (child) {
-            result = result.concat(child);
-        }
-
-        prevEndLine = pos.end.line;
-        prevOffset = pos.end.offset;
-    }
-
-    return result;
-}
-
-/**
- * Convert `node` into NLCST.
- *
- * @param {MDASTNode} node - Node.
- * @param {number?} index - Position of `node` in `parent`.
- * @param {MDASTNode?} parent - Parent node of `node`.
- * @param {File} file - Virtual file.
- * @param {Object} location - Bound location info.
- * @param {Parser} parser - NLCST parser.
- * @return {Array.<NLCSTNode>?} - A list of NLCST nodes, if
- *   `node` could be converted.
- */
-function one(node, index, parent, file, location, parser) {
-    var type = node.type;
-    var pos = node.position;
-    var start = location.toOffset(pos.start);
-    var end = location.toOffset(pos.end);
-    var replacement;
-
-    if (type in IGNORE) {
-        return null;
-    }
-
-    if (node.children) {
-        replacement = all(node, file, location, parser);
-    } else if (
-        type === 'image' ||
-        type === 'imageReference'
-    ) {
-        replacement = patch(
-            parser.tokenize(node.alt), location, start + 2
-        );
-    } else if (
-        type === 'text' ||
-        type === 'escape'
-    ) {
-        replacement = patch(
-            parser.tokenize(node.value), location, start
-        );
-    } else if (node.type === 'break') {
-        replacement = patch([
-            parser.tokenizeWhiteSpace('\n')
-        ], location, start);
-    } else if (node.type === 'inlineCode') {
-        replacement = patch([parser.tokenizeSource(
-            file.toString().slice(start, end)
-        )], location, start);
-    }
-
-    return replacement || null;
-}
-
-/**
- * Transform `ast` into `nlcst`.
- *
- * @param {Node} tree - MDAST node.
- * @param {File} file - Virtual file.
- * @param {Parser|Function} Parser - (Instance of) NLCST
- *   parser.
- * @return {NLCSTNode} - NLCST.
- */
-function toNLCST(tree, file, Parser) {
-    var parser;
-    var location;
-
-    /* Warn for invalid parameters. */
-    if (!tree || !tree.type) {
-        throw new Error('mdast-util-to-nlcst expected node');
-    }
-
-    if (!file || !file.messages) {
-        throw new Error('mdast-util-to-nlcst expected file');
-    }
-
-    /* Construct parser. */
-    if (!Parser) {
-        throw new Error('mdast-util-to-nlcst expected parser');
-    }
-
-    location = vfileLocation(file);
-
-    if (
-        !tree.position ||
-        !tree.position.start ||
-        !tree.position.start.column ||
-        !tree.position.start.line
-    ) {
-        throw new Error('mdast-util-to-nlcst expected position on nodes');
-    }
-
-    parser = 'parse' in Parser ? Parser : new Parser();
-
-    /* Transform mdast into NLCST tokens, and pass these
-     * into `parser.parse` to insert sentences, paragraphs
-     * where needed. */
-    return parser.parse(one(tree, null, null, file, location, parser));
-}
-
-/* Expose. */
-module.exports = toNLCST;
--- test/index.js
+++ test/index.js
@@ -8,10 +8,6 @@
 
 'use strict';
 
-/* eslint-env node */
-/* jscs:disable jsDoc */
-/* jscs:disable maximumLineLength */
-
 /* Dependencies. */
 var fs = require('fs');
 var path = require('path');
@@ -21,6 +17,8 @@ var vfile = require('vfile');
 var Latin = require('parse-latin');
 var Dutch = require('parse-dutch');
 var English = require('parse-english');
+var negate = require('negate');
+var hidden = require('is-hidden');
 var toNLCST = require('..');
 
 /* Methods. */
@@ -35,165 +33,142 @@ var fixtures = fs.readdirSync(ROOT);
 
 /* Tests. */
 test('mdast-util-to-nlcst', function (t) {
-    t.throws(
-        function () {
-            toNLCST();
-        },
-        /mdast-util-to-nlcst expected node/,
-        'should fail when not given an AST'
-    );
+  t.throws(
+    function () {
+      toNLCST();
+    },
+    /mdast-util-to-nlcst expected node/,
+    'should fail when not given an AST'
+  );
 
-    t.throws(
-        function () {
-            toNLCST({});
-        },
-        /mdast-util-to-nlcst expected node/,
-        'should fail when not given an AST (#2)'
-    );
+  t.throws(
+    function () {
+      toNLCST({});
+    },
+    /mdast-util-to-nlcst expected node/,
+    'should fail when not given an AST (#2)'
+  );
 
-    t.throws(
-        function () {
-            toNLCST({ type: 'foo' });
-        },
-        /mdast-util-to-nlcst expected file/,
-        'should fail when not given a file'
-    );
+  t.throws(
+    function () {
+      toNLCST({type: 'foo'});
+    },
+    /mdast-util-to-nlcst expected file/,
+    'should fail when not given a file'
+  );
 
-    t.throws(
-        function () {
-            toNLCST({ type: 'foo' });
-        },
-        /mdast-util-to-nlcst expected file/,
-        'should fail when not given a file (#2)'
-    );
+  t.throws(
+    function () {
+      toNLCST({type: 'foo'});
+    },
+    /mdast-util-to-nlcst expected file/,
+    'should fail when not given a file (#2)'
+  );
 
-    t.throws(
-        function () {
-            toNLCST(
-                { type: 'text', value: 'foo' },
-                { foo: 'bar' }
-            );
-        },
-        /mdast-util-to-nlcst expected file/,
-        'should fail when not given a file (#3)'
-    );
+  t.throws(
+    function () {
+      toNLCST(
+        {type: 'text', value: 'foo'},
+        {foo: 'bar'}
+      );
+    },
+    /mdast-util-to-nlcst expected file/,
+    'should fail when not given a file (#3)'
+  );
 
-    t.throws(
-        function () {
-            toNLCST(
-                { type: 'text', value: 'foo' },
-                vfile('foo')
-            );
-        },
-        /mdast-util-to-nlcst expected parser/,
-        'should fail without parser'
-    );
+  t.throws(
+    function () {
+      toNLCST(
+        {type: 'text', value: 'foo'},
+        vfile('foo')
+      );
+    },
+    /mdast-util-to-nlcst expected parser/,
+    'should fail without parser'
+  );
 
-    t.throws(
-        function () {
-            toNLCST({ type: 'text', value: 'foo' }, vfile(), Latin);
-        },
-        /mdast-util-to-nlcst expected position on nodes/,
-        'should fail when not given positional information'
-    );
+  t.throws(
+    function () {
+      toNLCST({type: 'text', value: 'foo'}, vfile(), Latin);
+    },
+    /mdast-util-to-nlcst expected position on nodes/,
+    'should fail when not given positional information'
+  );
 
-    t.doesNotThrow(
-        function () {
-            toNLCST({
-                type: 'text',
-                value: 'foo',
-                position: {
-                    start: {
-                        line: 1,
-                        column: 1
-                    },
-                    end: {
-                        line: 1,
-                        column: 4
-                    }
-                }
-            }, vfile(), English);
-        },
-        'should accept a parser constructor'
-    );
+  t.doesNotThrow(
+    function () {
+      toNLCST({
+        type: 'text',
+        value: 'foo',
+        position: {
+          start: {line: 1, column: 1},
+          end: {line: 1, column: 4}
+        }
+      }, vfile(), English);
+    },
+    'should accept a parser constructor'
+  );
 
-    t.doesNotThrow(
-        function () {
-            toNLCST({
-                type: 'text',
-                value: 'foo',
-                position: {
-                    start: {
-                        line: 1,
-                        column: 1
-                    },
-                    end: {
-                        line: 1,
-                        column: 4
-                    }
-                }
-            }, vfile(), new Dutch());
-        },
-        'should accept a parser instance'
-    );
+  t.doesNotThrow(
+    function () {
+      toNLCST({
+        type: 'text',
+        value: 'foo',
+        position: {
+          start: {line: 1, column: 1},
+          end: {line: 1, column: 4}
+        }
+      }, vfile(), new Dutch());
+    },
+    'should accept a parser instance'
+  );
 
-    t.throws(
-        function () {
-            toNLCST({
-                type: 'text',
-                value: 'foo',
-                position: {
-                    start: {},
-                    end: {}
-                }
-            }, vfile(), Latin);
-        },
-        /mdast-util-to-nlcst expected position on nodes/,
-        'should fail when not given positional information (#2)'
-    );
+  t.throws(
+    function () {
+      toNLCST({
+        type: 'text',
+        value: 'foo',
+        position: {start: {}, end: {}}
+      }, vfile(), Latin);
+    },
+    /mdast-util-to-nlcst expected position on nodes/,
+    'should fail when not given positional information (#2)'
+  );
 
-    t.test('should accept nodes without offsets', function (st) {
-        var node = toNLCST({
-            type: 'text',
-            value: 'foo',
-            position: {
-                start: {
-                    line: 1,
-                    column: 1
-                },
-                end: {
-                    line: 1,
-                    column: 4
-                }
-            }
-        }, vfile('foo'), Latin);
+  t.test('should accept nodes without offsets', function (st) {
+    var node = toNLCST({
+      type: 'text',
+      value: 'foo',
+      position: {
+        start: {line: 1, column: 1},
+        end: {line: 1, column: 4}
+      }
+    }, vfile('foo'), Latin);
 
-        st.equal(node.position.start.offset, 0, 'should set starting offset');
-        st.equal(node.position.end.offset, 3, 'should set ending offset');
+    st.equal(node.position.start.offset, 0, 'should set starting offset');
+    st.equal(node.position.end.offset, 3, 'should set ending offset');
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.end();
+  t.end();
 });
 
 /* Fixtures. */
 test('Fixtures', function (t) {
-    fixtures
-        .filter(function (filepath) {
-            return filepath.indexOf('.') !== 0;
-        })
-        .forEach(function (fixture) {
-            var filepath = join(ROOT, fixture);
-            var output = read(join(filepath, 'output.json'), 'utf-8');
-            var input = read(join(filepath, 'input.md'), 'utf-8');
+  fixtures
+    .filter(negate(hidden))
+    .forEach(function (fixture) {
+      var filepath = join(ROOT, fixture);
+      var output = read(join(filepath, 'output.json'), 'utf-8');
+      var input = read(join(filepath, 'input.md'), 'utf-8');
 
-            t.deepEqual(
-                toNLCST(remark().parse(input), vfile(input), Latin),
-                JSON.parse(output),
-                'should work on `' + fixture + '`'
-            );
-        });
+      t.deepEqual(
+        toNLCST(remark().parse(input), vfile(input), Latin),
+        JSON.parse(output),
+        'should work on `' + fixture + '`'
+      );
+    });
 
-    t.end();
+  t.end();
 });
